28f97d437cd667ea53b425fab7fa5c16b06faa29
JPV2207 <111731727+JPV2207@users.noreply.github.com>
2024-05-03 01:07:36 +0530

    Add the dapp frontend files

---
+++ script.js
@@ -0,0 +1,63 @@
+
+  src="https://cdn.ethers.io/lib/ethers-5.7.2.umd.min.js"
+  type="application/javascript"
+
+
+// Replace the following two values
+const MoodContractAddress = 0x28C0Bbdf30803D5D5bc46BA805F5f1F0B8FA66BB;
+const MoodContractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_mood",
+				"type": "string"
+			}
+		],
+		"name": "setMood",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getMood",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const provider = new ethers.providers.Web3Provider(window.ethereum, "sepolia");
+
+// Currently these two are undefined, we will use Ethers to assign them values
+let MoodContract = undefined;
+let signer = undefined;
+
+provider.send("eth_requestAccounts", []).then(() => {
+  provider.listAccounts().then((accounts) => {
+    signer = provider.getSigner(accounts[0]);
+    MoodContract = new ethers.Contract(
+      MoodContractAddress,
+      MoodContractABI,
+      signer
+    );
+  });
+});
+
+        async function getMood() {
+  const mood = await MoodContract.getMood();
+  document.getElementById("showMood").innerText = `Your Mood: ${mood}`;
+  console.log(mood);
+}
+
+async function setMood() {
+  const mood = document.getElementById("mood").value;
+  await MoodContract.setMood(mood);
+} 
